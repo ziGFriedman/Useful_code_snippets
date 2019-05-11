3d1bb73744996c90f08c984260b6c1415ea1a15b
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-05-11 14:38:09 +0500

    Create Spread.py

---
+++ Lists/Spread.py
@@ -0,0 +1,12 @@
+'''Заимствует [].concat(…arr) из Javascript. Сглаживает список (не глубоко) и
+возвращает новый список.'''
+def spread(arg):
+    ret = []
+    for i in arg:
+        if isinstance(i, list):
+            ret.extend(i)
+        else:
+            ret.append(i)
+    return ret
+
+print(spread([1,2,3,[4,5,6],[7],8,9]))
