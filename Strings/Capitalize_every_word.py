7460aedf16815ce205cc63470afa9d880ed68660
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-05-11 15:13:09 +0500

    Create Capitalize_every_word.py

---
+++ Strings/Capitalize_every_word.py
@@ -0,0 +1,5 @@
+'''Делает первую букву заглавной каждого слова строки. Используется str.title.'''
+def capitalize_every_word(string):
+    return string.title()
+
+print(capitalize_every_word('hello world!'))
