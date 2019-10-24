0db72a6e15839444327e641a77c3311464455596
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-10-24 21:49:45 +0500

    Update Сount_occurences.py

--- Lists/Сount_occurences.py
+++ Lists/Сount_occurences.py
@@ -6,5 +6,5 @@ from functools import reduce
 
 def count_occurences(arr, val):
     return reduce((lambda x, y: x + 1 if y == val and type(y) == type(val) else x + 0), arr)
-
+ 
 print(count_occurences([1, 1, 2, 1, 2, 3], 1))
